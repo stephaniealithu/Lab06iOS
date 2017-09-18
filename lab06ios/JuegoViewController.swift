e4136d0bb150d4aa7f74e4a3267d023e3cc676ba
Steph <stephanie.granados@tecsup.edu.pe>
2017-09-18 17:40:22 -0500

    v1

---
+++ lab06ios/JuegoViewController.swift
@@ -0,0 +1,50 @@
+//
+//  JuegoViewController.swift
+//  lab06ios
+//
+//  Created by Mac Tecsup on 18/09/17.
+//  Copyright © 2017 Mac Tecsup. All rights reserved.
+//
+
+import UIKit
+
+class JuegoViewController: UIViewController , UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+
+    @IBOutlet weak var JuegoImageView: UIImageView!
+    @IBOutlet weak var tituloTextField: UITextField!
+    
+    var imagePicker = UIImagePickerController()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate=self
+  
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func fotosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion:  nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let imagenSeleccionada = info[UIImagePickerControllerOriginalImage] as! UIImage
+        JuegoImageView.image = imagenSeleccionada
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func camaraTapped(_ sender: Any) {
+    }
+ 
+    @IBAction func agregarTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let juego = Juego(context: context)
+        juego.titulo = tituloTextField.text
+        juego.imagen = UIImagePNGRepresentation(JuegoImageView.image!) as NSData?
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController!.popViewController(animated: true)
+    }
+    
+
+    
+}
